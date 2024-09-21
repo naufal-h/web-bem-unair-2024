a95c6a2c7677bf03e6b62bf6648dffcc00cc6e01
naufal-h <naufal.humam-2021@fst.unair.ac.id>
2024-09-21 21:05:07 +0700

    feat: Add FourthSection component to About page

--- resources/js/Components/Card/CardLogo.tsx
+++ resources/js/Components/Card/CardLogo.tsx
@@ -1,69 +1,75 @@
 // ** MUI Imports
-import Card from "@mui/material/Card";
-import Typography from "@mui/material/Typography";
-import CardContent from "@mui/material/CardContent";
 import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
 
 // ** Custom Components Imports
 
 type CardLogoProps = {
-  image: string;
-  text: string;
+    image: string;
+    text: string;
 };
 
 const CardLogo = ({ image, text }: CardLogoProps) => {
-  return (
-    <Card
-      sx={{
-        maxWidth: "350px",
-        width: "250px",
-        minWidth: "100px",
-        backgroundColor: "#FFF9F9",
-        borderRadius: "10px",
-        display: "flex",
-        flexDirection: "column",
-        transition: "background-color 0.3s, transform 0.3s",
-        "&:hover": {
-          backgroundColor: "#850E09",
-          transform: "scale(1.02)",
-          "& .headline-text": {
-            color: "#FFF9F9",
-          },
-          "& .description-text": {
-            color: "#FFF9F9",
-          },
-        },
-      }}
-    >
-      <CardContent
-        sx={{
-          display: "flex",
-          textAlign: "center",
-          alignItems: "center",
-          flexDirection: "column",
-          p: (theme) => `${theme.spacing(8.75, 2, 9.25)} !important`,
-        }}
-      >
-        <Box sx={{ 
-          width: "75%",
-         }} component="img" src={image} alt={text} />
-
-        <Typography
-          className="headline-text"
-          sx={{
-            width: "100%",
-            color: "#D0757D",
-            fontWeight: "bold",
-            fontFamily: "Sansation",
-            transition: "color 0.3s",
-          }}
-          variant="h6"
+    return (
+        <Card
+            sx={{
+                maxWidth: "380px",
+                minWidth: "100px",
+                maxHeight: "280px",
+                backgroundColor: "#FFF9F9",
+                borderRadius: "10px",
+                display: "flex",
+                flexDirection: "column",
+                transition: "background-color 0.3s, transform 0.3s",
+                "&:hover": {
+                    backgroundColor: "#850E09",
+                    transform: "scale(1.02)",
+                    "& .headline-text": {
+                        color: "#FFF9F9",
+                    },
+                    "& .description-text": {
+                        color: "#FFF9F9",
+                    },
+                },
+            }}
         >
-          {text}
-        </Typography>
-      </CardContent>
-    </Card>
-  );
+            <CardContent
+                sx={{
+                    display: "flex",
+                    textAlign: "center",
+                    alignItems: "center",
+                    flexDirection: "column",
+                    p: (theme) => `${theme.spacing(2.5, 2, 7.5)} !important`,
+                }}
+            >
+                <Box
+                    sx={{
+                        width: { xs: "100%", md: "50%", lg: "50%" },
+                    }}
+                    component="img"
+                    src={image}
+                    alt={text}
+                />
+
+                <Typography
+                    className="headline-text"
+                    sx={{
+                        width: "100%",
+                        color: "#211D1D",
+                        fontWeight: "bold",
+                        fontFamily: "Sansation",
+                        transition: "color 0.3s",
+                        fontSize: { xs: "1rem", md: "1.5rem" },
+                    }}
+                    variant="h5"
+                >
+                    {text}
+                </Typography>
+            </CardContent>
+        </Card>
+    );
 };
 
 export default CardLogo;
