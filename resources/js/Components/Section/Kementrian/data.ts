b82e688301324772a806456d94ff82cbee824e12
safranatha <safranatha@gmail.com>
2024-10-15 06:39:32 +0700

    anggota done

--- resources/js/Components/Section/Kementrian/data.ts
+++ resources/js/Components/Section/Kementrian/data.ts
@@ -111,7 +111,25 @@ export const KemenkoanList: MainKemenkoanInterface[] = [
                         deskripsi_proker: "",
                     },
                 ],
-                anggota: [],
+                anggota: [
+                    {
+                        image: "/images/fungsionaris/PENGPROF/Dirjen Relasi Korporasi_REYVO WILIAM.png",
+                        nama: "REYVO WILLIAM",
+                        jabatan: "DIRJEN RELASI KORPORASI",
+
+
+                    },
+                    {
+                        image: "/images/fungsionaris/PENGPROF/Menteri PENGPROF_KHOLIS HAFIDLOTUL.png",
+                        nama: "KHOLISH HAFIDLOTUL",
+                        jabatan: "MENTERI PENGEMBANGAN PROFESI",
+                    },
+                    {
+                        image: "/images/fungsionaris/PENGPROF/Dirjen Pengembangan Keahlian_NABILA AILSHA.png",
+                        nama: "NABILA AILSHA",
+                        jabatan: "DIRJEN PENGEMBANGAN KEAHLIAN",
+                    },
+                ],
             },
             {
                 nama: "KEMENTRIAN PSDM",
@@ -123,7 +141,21 @@ export const KemenkoanList: MainKemenkoanInterface[] = [
                         deskripsi_proker: "",
                     },
                 ],
-                anggota: [],
+                anggota: [
+                    {
+                        image: "/images/fungsionaris/PSDM/Dirjen Kaderisasi Mahasiswa_RIFKI APRIALDI.png",
+                        nama: "RIFKI APRIALDI",
+                        jabatan: "DIRJEN KADERISASI MAHASISWA",
+                    },
+                    {
+                        image: "/images/fungsionaris/PSDM/Menteri PSDM_JAGAD MARSELA.png",
+                        nama: "JAGAD MARSELA",
+                        jabatan: "MENTERI PSDM",
+                    }, {
+                        image: "/images/fungsionaris/PSDM/Dirjen Akselerasi Potensi Mahasiswa_AMARE AMODIA.png",
+                        nama: "AMARE AMODIA",
+                        jabatan: "DIRJEN AKSELERASI POTENSI MAHASISWA",
+                    },],
             },
             {
                 nama: "KEMENTRIAN ADKESMA",
@@ -135,12 +167,28 @@ export const KemenkoanList: MainKemenkoanInterface[] = [
                         deskripsi_proker: "",
                     },
                 ],
-                anggota: [],
-            },
+                anggota: [
+                    {
+                        image: "/images/fungsionaris/ADKESMA/Dirjen Advokasi Kebijakan Kampus_FARA SABHRINA.png",
+                        nama: "FARA SHABRINA",
+                        jabatan: "DIRJEN ADVOKASI KEBIJAKAN KAMPUS",
+                    },
+                    {
+                        image: "/images/fungsionaris/ADKESMA/Menteri ADKESMA_SHINTYA IFTITAH.png",
+                        nama: "SHINTYA IFTITAH",
+                        jabatan: "MENTERI ADKESMA",
+                    },
+                    {
+                        image:"/images/fungsionaris/ADKESMA/Dirjen Kesejahteraan Mahasiswa_RIKY EKA.png",
+                        nama: "RIKY EKA",
+                        jabatan: "DIRJEN KESEJAHTERAAN MAHASISWA",
+                    }
+                ],
+            }
         ],
         nama_kementrian: "KEMENKOAN KEMAHASISWAAN",
         text_kementrian:
-            "Cillum amet sint cupidatat ut ad dolore ipsum deserunt pariatur. Incididunt adipisicing amet aute nulla officia magna deserunt ut qui reprehenderit enim irure.",
+            "",
         id: "kemahasiswaan",
     },
     {
@@ -158,7 +206,24 @@ export const KemenkoanList: MainKemenkoanInterface[] = [
                         deskripsi_proker: "",
                     },
                 ],
-                anggota: [],
+                anggota: [
+                    {
+                        image:"/images/fungsionaris/KESEHATAN/Dirjen Pencegahan dan  Pengendalian Penyakit_CHANDRA DWWI.png",
+                        nama: "CHANDRA DWI WIJAYA",
+                        jabatan: "DIRJEN PENCEGAHAN DAN PENGENDALIAN PENYAKIT",
+                    },
+                    {
+                        image: "/images/fungsionaris/KESEHATAN/Menteri Kesehatan_YUSILAH.png",
+                        nama: "YUSILAH",
+                        jabatan: "MENTERI KESEHATAN",
+                    },
+                    {
+                        image: "/images/fungsionaris/KESEHATAN/Dirjen Psikosia  dan Inklusifitas_ANISA FARADILA.png",
+                        nama: "ANISA FARADILA",
+                        jabatan: "DIRJEN PSIKOSIA DAN INKLUSIFITAS",
+                    }
+                    
+                ],
             },
             {
                 nama: "KEMENTRIAN PENGABDIAN MASYARAKAT",
@@ -170,7 +235,28 @@ export const KemenkoanList: MainKemenkoanInterface[] = [
                         deskripsi_proker: "",
                     },
                 ],
-                anggota: [],
+                anggota: [
+                    {
+                        image: "/images/fungsionaris/PENGMAS/Dirjen Pembangunan Generasi Muda_FIKRI FARISI.png",
+                        nama: "FIKRI FARISI",
+                        jabatan: "DIRJEN PEMBANGUNAN GENERASI MUDA",
+                    },
+                    {
+                        image: "/images/fungsionaris/PENGMAS/Menteri PENGMAS_BENOIT HAKEEM.png",
+                        nama: "BENOIT HAKEEM",
+                        jabatan: "MENTERI PENGMAS",
+                    },
+                    {
+                        image: "/images/fungsionaris/PENGMAS/Dirjen Pengembangan Desa Mitra_MAULANA ANDHIKA.png",
+                        nama: "MAULANA ANDHIKA",
+                        jabatan: "DIRJEN PENGEMBANGAN DESA MITRA",
+                    },
+                    {
+                        image: "/images/fungsionaris/PENGMAS/Dirjen Sosial dan Masyarakat_KARINA PUTRI.png",
+                        nama: "KARINA PUTRI",
+                        jabatan: "DIRJEN SOSIAL DAN MASYARAKAT",
+                    }
+                ],
             },
             {
                 nama: "KEMENTRIAN LINGKUNGAN HIDUP",
@@ -182,12 +268,33 @@ export const KemenkoanList: MainKemenkoanInterface[] = [
                         deskripsi_proker: "",
                     },
                 ],
-                anggota: [],
+                anggota: [
+                    {
+                        image: "/images/fungsionaris/LH/Dirjen PPKL_RAYIKA WIDHI.png",
+                        nama: "RAYIKA WIDHI",
+                        jabatan: "DIRJEN PPKL",
+                    },
+                    {
+                        image:"/images/fungsionaris/LH/menteri LH_MUHAMMAD.png",
+                        nama: "MUHAMMAD",
+                        jabatan: "MENTERI LH",
+                    },
+                    {
+                        image:"/images/fungsionaris/LH/Dirjen Advokasi Kajian Strategis lingkungan_JASON ASIRO.png",
+                        nama: "JASON ASIRO",
+                        jabatan: "DIRJEN ADVOKASI KAJIAN STRATEGIS LINGKUNGAN",
+                    },
+                    {
+                        image:"/images/fungsionaris/LH/Dirjen Pengembangan Prservesi SDA_ARCELIA FERINDA.png",
+                        nama: "ARCELIA FERINDA",
+                        jabatan: "DIRJEN PENGEMBANGAN PRESERVASI SDA",
+                    }
+                ],
             },
         ],
         nama_kementrian: "KEMENKOAN KEMASYARAKATAN",
         text_kementrian:
-            "Cillum amet sint cupidatat ut ad dolore ipsum deserunt pariatur. Incididunt adipisicing amet aute nulla officia magna deserunt ut qui reprehenderit enim irure.",
+            "",
         id: "kemasyarakatan",
     },
     {
@@ -205,7 +312,23 @@ export const KemenkoanList: MainKemenkoanInterface[] = [
                         deskripsi_proker: "",
                     },
                 ],
-                anggota: [],
+                anggota: [
+                    {
+                        image:"/images/fungsionaris/SOSPOL/Dirjen Analisis Isu Strategis_AHMAD HARIST.png",
+                        nama: "AHMAD HARIST",
+                        jabatan: "DIRJEN ANALISIS ISU STRATEGIS",
+                    },
+                    {
+                        image:"/images/fungsionaris/SOSPOL/Menteri sospol_ERDOGAN THAYYIB.png",
+                        nama: "ERDOGAN THAYYIB",
+                        jabatan: "MENTERI SOSPOL",
+                    },
+                    {
+                        image:"/images/fungsionaris/SOSPOL/Dirjen Aksi dan Propaganda_ILHAM HERALIZADI.png",
+                        nama: "ILHAM HERALIZADI",
+                        jabatan: "DIRJEN AKSI DAN PROPAGANDA",
+                    }
+                ],
             },
             {
                 nama: "KEMENTRIAN KESETARAAN GENDER",
@@ -216,7 +339,23 @@ export const KemenkoanList: MainKemenkoanInterface[] = [
                         deskripsi_proker: "",
                     },
                 ],
-                anggota: [],
+                anggota: [
+                    {
+                        image:"/images/fungsionaris/KG/Dirjen Pemberdayaan Perempuan Progresif_TSURAYYA KAMILAH.png",
+                        nama: "TSURAYYA KAMILAH",
+                        jabatan: "DIRJEN PEMBERDAYAAN PEREMPUAN PROGRESIF",
+                    },
+                    {
+                        image:"/images/fungsionaris/KG/Menteri Kesetaraan Gender_DIHAN ASNAFIN.png",
+                        nama: "DIHAN ASNAFIN",
+                        jabatan: "MENTERI KESETARAAN GENDER",
+                    },
+                    {
+                        image:"/images/fungsionaris/KG/Dirjen Edukasi dan Aksi_AZLIN HUWAIDA.png",
+                        nama: "AZLIN HUWAIDA",
+                        jabatan: "DIRJEN EDUKASI DAN AKSI",
+                    }
+                ],
             },
         ],
         nama_kementrian: "KEMENKOAN PERGERAKAN",
@@ -237,7 +376,23 @@ export const KemenkoanList: MainKemenkoanInterface[] = [
                         deskripsi_proker: "",
                     },
                 ],
-                anggota: [],
+                anggota: [
+                    {
+                        image:"/images/fungsionaris/RISKEL/Dirjen Riset dan Inovasi_BAGAS DIMAS.png",
+                        nama: "BAGAS DIMAS",
+                        jabatan: "DIRJEN RISET DAN INOVASI",
+                    },
+                    {
+                        image:"/images/fungsionaris/RISKEL/Menteri Riset dan Keilmuan_SHINTA ADELLYA.png",
+                        nama: "SHINTA ADELLYA",
+                        jabatan: "MENTERI RISET DAN KEILMUAN",
+                    },
+                    {
+                        image:"/images/fungsionaris/RISKEL/Dirjen Akademik dan Prestasi_AHMAD RIZQI.png",
+                        nama: "AHMAD RIZQI",
+                        jabatan: "DIRJEN AKADEMIK DAN PRESTASI",
+                    }
+                ],
             },
             {
                 nama: "KEMENTRIAN SENI DAN OLAHRAGA",
@@ -248,7 +403,23 @@ export const KemenkoanList: MainKemenkoanInterface[] = [
                         deskripsi_proker: "",
                     },
                 ],
-                anggota: [],
+                anggota: [
+                    {
+                        image:"/images/fungsionaris/SENIORA/Dirjen Kesehatan Jasmani  dan Prestasi Olahraga_JOHANES RICHARD.png",
+                        nama: "JOHANES RICHARD",
+                        jabatan: "DIRJEN KESEHATAN JASMIAN DAN PRESTASI OLAHRAGA",
+                    },
+                    {
+                        image:"/images/fungsionaris/SENIORA/Menteri SENIORA_APTA AMALIA.png",
+                        nama: "APTA AMALIA",
+                        jabatan: "MENTERI SENIORA",
+                    },
+                    {
+                        image:"/images/fungsionaris/SENIORA/Dirjen Ekspresi, Seni, dan Kebudayaan_DERI BAYU.png",
+                        nama: "DERI BAYU",
+                        jabatan: "DIRJEN EKSPRESI, SENI, DAN KEBUDAYAAN",
+                    }
+                ],
             },
             {
                 nama: "KEMENTRIAN EDIGPRO",
@@ -259,7 +430,23 @@ export const KemenkoanList: MainKemenkoanInterface[] = [
                         deskripsi_proker: "",
                     },
                 ],
-                anggota: [],
+                anggota: [
+                    {
+                        image:"/images/fungsionaris/EDIGPRO/Dirjen Investasi dan  Industri Kreatif_ABID OMAR.png",
+                        nama: "ABID OMAR",
+                        jabatan: "DIRJEN INVESTASI DAN INDUSTRI KREATIF",
+                    },
+                    {
+                        image:"/images/fungsionaris/EDIGPRO/Menteri EDIGPRO_AMIN MADANI.png",
+                        nama: "AMIN MADANI",
+                        jabatan: "MENTERI EDIGPRO",
+                    },
+                    {
+                        image:"/images/fungsionaris/EDIGPRO/Dirjen UMKM dan  Kewirausahaan_SHALOM DAVITA.png",
+                        nama: "SHALOM DAVITA",
+                        jabatan: "DIRJEN UMKM DAN KEWIRA BUSINESSES",
+                    }
+                ],
             },
         ],
         nama_kementrian: "KEMENKOAN PPK",
